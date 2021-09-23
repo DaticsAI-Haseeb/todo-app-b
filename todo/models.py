d4b94ec4a6e431be2ce0f1a5029312c370228f6a
Haseeb Arshad <haseeb.arshad@datics.ai>
2021-09-23 11:56:07 +0500

    first commit

---
+++ todo/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+# Create your models here.
+
+# class User(models.Model):
+#     name =
+
+
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=255)
+    date_created = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    # priority
+
+    # created_by
+
+
+
+# class Description(models.Model):
